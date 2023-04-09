6532d2bf43026dcbce95dedba19325b0ecc91197
Moise Bisimwa <121420945+Moses14141@users.noreply.github.com>
2023-04-09 16:06:22 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let fighting;
+let monterhealth;
+let inventory = ["stick"]
+
+const button1 = document 
+const button2  document.querySelector
+const button3
+const button
+const text button = document.querySelector
+const button
+const button
+const button
+const button
+const button
+const button
+
